3d749d18e161573e06d84600d4e0d66d510a561b
xupingwei <mailxpw@gmail.com>
2019-06-28 12:07:48 +0800

    更换为elementui

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,62 +1,103 @@
 <template>
-    <div class="container-fluid p-0 row bg-dark">
-        <div class="col-sm-4 m-auto">
-            <nav class="navbar navbar-expand-sm  navbar-dark m-0">
-                <img src="../assets/avatar.jpg" class="rounded float-left mr-3" width="40" height="40" alt="头像">
-                <form class="form-control-range m-auto">
-                    <div class="input-group">
-                        <input class="form-control" type="text" placeholder="Search or jump to...">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary border-1" type="button">搜索</button>
-                        </div>
-                    </div>
-                </form>
-            </nav>
-        </div>
-        <div class="col-sm-4">
-            <nav class="navbar navbar-expand-sm  navbar-dark m-0">
-                <ul class="navbar-nav">
-                    <li class="nav-item mr-3">
-                        <router-link tag="a" :to="{name:'homeLink'}" class="nav-link">首页
-                        </router-link>
-                    </li>
-                    <li class="nav-item mr-3">
-                        <router-link class="nav-link" tag="a" :to="{name:'articleLink'}">文章</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" tag="a" :to="{name:'aboutLink'}">关于我</router-link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <div class="col-sm-1"></div>
-        <div class="col-sm-3">
-            <nav class="navbar navbar-expand-sm  navbar-dark float-right">
-                <ul class="navbar-nav mt-2">
-                    <li class="nav-item active">
-                        <a href="#"><img src="../assets/fluidicon.png" class="rounded float-left mr-2"
-                                         width="20" height="20" alt="github"></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#"><img src="../assets/jianshu.png" class="rounded float-left mr-2"
-                                         width="20" height="20" alt="简书"></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#"><img src="../assets/favicon.png" class="rounded float-left mr-2"
-                                         width="20" height="20" alt="CSDN"></a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </div>
+    <el-header>
+        <el-row>
+            <el-col :span="8">
+                <div class="search">
+                    <a href="#">
+                        <el-image :src="headerUrl"
+                                  style="height: 40px;width: 40px;"
+                                  fit="cover"></el-image>
+                    </a>
+                    <el-input style="width: 80%;" placeholder="请输入内容" v-model="searchContent">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </div>
+            </el-col>
+            <el-col :span="6" offset="1">
+                <el-menu :default-active="$route.path"
+                         class="el-menu-demo"
+                         mode="horizontal"
+                         background-color="#545c64"
+                         text-color="#fff"
+                         active-text-color="#ffd04b">
+                    <el-menu-item index="1">
+                        <router-link tag="li" :to="{name:'homeLink'}">首页</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="2">
+                        <router-link tag="li" :to="{name:'articleLink'}">文章</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="3">
+                        <router-link tag="li" :to="{name:'aboutLink'}">关于我</router-link>
+                    </el-menu-item>
+                </el-menu>
+            </el-col>
+
+            <el-col :span="4" offset="5">
+                <el-menu mode="horizontal"
+                         background-color="#545c64"
+                         text-color="#fff"
+                         active-text-color="#ffd04b">
+                    <el-menu-item index="4">
+                        <a href="#">
+                            <img src="../assets/fluidicon.png"
+                                 width="20" height="20" alt="github"></a>
+                    </el-menu-item>
+                    <el-menu-item index="5">
+                        <a href="#">
+                            <img src="../assets/jianshu.png"
+                                 width="20" height="20" alt="简书"></a>
+                    </el-menu-item>
+                    <el-menu-item index="6">
+                        <a href="#">
+                            <img src="../assets/favicon.png"
+                                 width="20" height="20" alt="CSDN"></a>
+                    </el-menu-item>
+                </el-menu>
+            </el-col>
+        </el-row>
+    </el-header>
 </template>
 
 <script>
     export default {
-        name: "Header"
+        name: "Header",
+        data() {
+            return {
+                // eslint-disable-next-line no-undef
+                headerUrl: "../assets/avatar.jpg",
+                searchContent: ""
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .el-row {
+        width: 100%;
+    }
 
+    .el-header {
+        margin: 0;
+        padding: 0;
+        background-color: #545c64;
+        display: flex;
+    }
+
+    .search {
+        height: 90px;
+        line-height: 90px;
+        flex-direction: row;
+    }
+
+    .search > a {
+        margin-left: 20px;
+        float: left;
+    }
+
+    .search > .el-input {
+        float: left;
+        margin-left: 10px;
+        border: 1px solid #545c64;
+        margin-top: 10px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
-
 
 new Vue({
     router,

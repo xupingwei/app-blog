0b6a093a0a53225e8f681b3bb0bc9ffffe25eee8
xupingwei <mailxpw@gmail.com>
2019-07-01 19:00:32 +0800

    完成发布管理后台

--- src/views/ArticleDetails.vue
+++ src/views/ArticleDetails.vue
@@ -1,5 +1,7 @@
 <template>
-
+    <div class="article-details">
+        <h1>文章详情</h1>
+    </div>
 </template>
 
 <script>
@@ -9,5 +11,7 @@
 </script>
 
 <style scoped>
+    .article-details {
 
+    }
 </style>

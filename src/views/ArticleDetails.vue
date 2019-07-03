af0baabeee59d74e0ea0b76d71d070e75ad20540
xupingwei <mailxpw@gmail.com>
2019-07-03 18:21:27 +0800

    开发文章详情页面

--- src/views/ArticleDetails.vue
+++ src/views/ArticleDetails.vue
@@ -1,14 +1,33 @@
 <template>
     <div class="article-details">
         <el-row :gutter="10">
-            <el-col :span="13" :offset="1">
-                <h3>这是标题这是标题这是标题</h3>
+            <el-col :span="14" :offset="1">
+                <el-card>
+                    <h2>这是标题这是标题这是标题</h2>
+                    <div class="article-left-info">
+                        <p>2019年07月02日 18:00:35</p>
+                        <div class="key-word">
+                            <p>关键字：</p>
+                            <el-tag>Java</el-tag>
+                            <el-tag>Android</el-tag>
+                            <el-tag>持续集成</el-tag>
+                        </div>
+                    </div>
+                    <div class="article-classify">
+                        <p>个人分类：</p>
+                        <el-link type="primary">Android设计模式</el-link>
+                        <el-link type="primary">java设计模式</el-link>
+                    </div>
+                </el-card>
             </el-col>
-            <el-col :span="8" :offset="2">
+            <el-col :span="8" :offset="1">
                 <el-card>
                     <div>
-                        <el-avatar icon="el-icon-user-solid"></el-avatar>
-                        <!--                        <h4>博客专家</h4>-->
+                        <el-avatar :size="60"
+                                   :src="headerUrl"
+                                   icon="el-icon-user-solid">
+                        </el-avatar>
+                        <h5>守住本心，保持本源;</h5>
                     </div>
                     <el-divider></el-divider>
                     <template>
@@ -27,13 +46,19 @@
                     <template>
                         <el-row class="details-right-icon">
                             <el-col :span="8">
-                                <el-button type="primary" icon="el-icon-chat-line-round" circle></el-button>
+                                <el-tooltip class="item" effect="dark" content="打开QQ留言?" placement="top">
+                                    <el-button type="primary" icon="el-icon-chat-round" circle></el-button>
+                                </el-tooltip>
                             </el-col>
                             <el-col :span="8">
-                                <el-button type="primary" icon="el-icon-message" circle></el-button>
+                                <el-tooltip class="item" effect="dark" content="发送邮件留言?" placement="top">
+                                    <el-button type="primary" icon="el-icon-message" circle></el-button>
+                                </el-tooltip>
                             </el-col>
                             <el-col :span="8">
-                                <el-button type="primary" icon="el-icon-chat-dot-round" circle></el-button>
+                                <el-tooltip class="item" effect="dark" content="本站内留言?" placement="top">
+                                    <el-button type="primary" icon="el-icon-chat-dot-square" circle></el-button>
+                                </el-tooltip>
                             </el-col>
                         </el-row>
                     </template>
@@ -45,7 +70,12 @@
 
 <script>
     export default {
-        name: "ArticleDetails"
+        name: "ArticleDetails",
+        data() {
+            return {
+                headerUrl: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4259300811,497831842&fm=26&gp=0.jpg"
+            }
+        }
     }
 </script>
 
@@ -69,5 +99,58 @@
         text-align: center;
         font-size: 18px;
         margin-top: 10px;
+    }
+
+    .el-avatar {
+        display: block;
+        margin: 0 auto;
+    }
+
+    h5 {
+        margin: 10px auto;
+        text-align: center;
+    }
+
+    h2 {
+        text-align: center;
+    }
+
+    .article-left-info {
+        margin-top: 20px;
+        display: flex;
+        height: 40px;
+        line-height: 40px;
+    }
+
+    .article-left-info p {
+        color: gray;
+        font-size: 14px;
+    }
+
+    .key-word {
+        display: inline-flex;
+        margin-left: 20px;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+    }
+
+    .el-tag {
+        margin-left: 10px;
+        height: 30px;
+        text-align: center;
+        margin-top: 5px;
+    }
+
+    .article-classify {
+        font-size: 12px;
+        color: gray;
+        margin-top: 10px;
+        display: flex;
+    }
+
+    .el-link{
+        font-size: 12px;
+        margin-left: 15px;
     }
 </style>

1960d11d4b600bddbc3e18cfc944dceba9099f17
xupingwei <mailxpw@gmail.com>
2019-07-03 23:38:33 +0800

    页面跳转

--- src/components/HomeArticleBrief.vue
+++ src/components/HomeArticleBrief.vue
@@ -1,74 +1,17 @@
 <template>
     <div class="article-brief">
         <el-timeline>
-            <el-timeline-item timestamp="2018/4/12" placement="top">
+            <el-timeline-item v-for="article in articleList" :key="article"
+                              :timestamp="article.pubTime"
+                              placement="top">
                 <el-card>
                     <div class="article-brief-title">
                         <h4>
-                            <el-link>更新 Github 模板更新 Github 模板更新 Github 模板</el-link>
+                            <el-link>{{article.title}}</el-link>
                         </h4>
-                        <el-link>详情<i class="el-icon-view el-icon--right"></i></el-link>
+                        <el-link @click="jumpDetails()">详情<i class="el-icon-view el-icon--right"></i></el-link>
                     </div>
-                    <p>王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                    </p>
-                </el-card>
-            </el-timeline-item>
-            <el-timeline-item timestamp="2018/4/12" placement="top">
-                <el-card>
-                    <div class="article-brief-title">
-                        <h4>
-                            <el-link>更新 Github 模板更新 Github 模板更新 Github 模板</el-link>
-                        </h4>
-                        <el-link>详情<i class="el-icon-view el-icon--right"></i></el-link>
-                    </div>
-                    <p>王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                    </p>
-                </el-card>
-            </el-timeline-item>
-            <el-timeline-item timestamp="2018/4/12" placement="top">
-                <el-card>
-                    <div class="article-brief-title">
-                        <h4>
-                            <el-link>更新 Github 模板更新 Github 模板更新 Github 模板</el-link>
-                        </h4>
-                        <el-link>详情<i class="el-icon-view el-icon--right"></i></el-link>
-                    </div>
-                    <p>王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                    </p>
-                </el-card>
-            </el-timeline-item>
-            <el-timeline-item timestamp="2018/4/12" placement="top">
-                <el-card>
-                    <div class="article-brief-title">
-                        <h4>
-                            <el-link>更新 Github 模板更新 Github 模板更新 Github 模板</el-link>
-                        </h4>
-                        <el-link>详情<i class="el-icon-view el-icon--right"></i></el-link>
-                    </div>
-                    <p>王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                    </p>
-                </el-card>
-            </el-timeline-item>
-            <el-timeline-item timestamp="2018/4/12" placement="top">
-                <el-card>
-                    <div class="article-brief-title">
-                        <h4>
-                            <el-link type="warning">更新 Github 模板更新 Github 模板更新 Github 模板</el-link>
-                        </h4>
-                        <el-link>详情<i class="el-icon-view el-icon--right"></i></el-link>
-                    </div>
-                    <p>王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                        王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46
-                    </p>
+                    <p>{{article.brief}}</p>
                 </el-card>
             </el-timeline-item>
         </el-timeline>
@@ -77,7 +20,94 @@
 
 <script>
     export default {
-        name: "article-brief"
+        name: "article-brief",
+        data() {
+            return {
+                articleList: [
+                    {
+                        title: "更新 Github 模板更新 Github 模板更新 Github 模板",   //标题
+                        comment: 0,  //评论
+                        browse: 0,    //浏览
+                        like: 0,    //点赞
+                        pubTime: "2018/4/12 13:45",  //发布时间
+                        brief: "王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46",  //文章简介
+                        content: "",   //文章内容
+                        author: "LiWei",  //作者
+                        keyWords: "java|android|go", //标签
+                        classify: "Android设计模式|java设计模式"   //文章分类
+                    },
+                    {
+                        title: "更新 Github 模板更新 Github 模板更新 Github 模板",   //标题
+                        comment: 0,  //评论
+                        browse: 0,    //浏览
+                        like: 0,    //点赞
+                        pubTime: "2018/4/12 13:45",  //发布时间
+                        brief: "王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46",  //文章简介
+                        content: "",   //文章内容
+                        author: "LiWei",  //作者
+                        keyWords: "java|android|go", //标签
+                        classify: "Android设计模式|java设计模式"   //文章分类
+                    },
+                    {
+                        title: "更新 Github 模板更新 Github 模板更新 Github 模板",   //标题
+                        comment: 0,  //评论
+                        browse: 0,    //浏览
+                        like: 0,    //点赞
+                        pubTime: "2018/4/12 13:45",  //发布时间
+                        brief: "王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46",  //文章简介
+                        content: "",   //文章内容
+                        author: "LiWei",  //作者
+                        keyWords: "java|android|go", //标签
+                        classify: "Android设计模式|java设计模式"   //文章分类
+                    },
+                    {
+                        title: "更新 Github 模板更新 Github 模板更新 Github 模板",   //标题
+                        comment: 0,  //评论
+                        browse: 0,    //浏览
+                        like: 0,    //点赞
+                        pubTime: "2018/4/12 13:45",  //发布时间
+                        brief: "王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46",  //文章简介
+                        content: "",   //文章内容
+                        author: "LiWei",  //作者
+                        keyWords: "java|android|go", //标签
+                        classify: "Android设计模式|java设计模式"   //文章分类
+                    },
+                    {
+                        title: "更新 Github 模板更新 Github 模板更新 Github 模板",   //标题
+                        comment: 0,  //评论
+                        browse: 0,    //浏览
+                        like: 0,    //点赞
+                        pubTime: "2018/4/12 13:45",  //发布时间
+                        brief: "王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46\n" +
+                            " 王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46王小虎 提交于 2018/4/3 20:46",  //文章简介
+                        content: "",   //文章内容
+                        author: "LiWei",  //作者
+                        keyWords: "java|android|go", //标签
+                        classify: "Android设计模式|java设计模式"   //文章分类
+                    }
+                ]
+            }
+        },
+        methods: {
+            jumpDetails() {
+                let routeUrl = this.$router.resolve({
+                    name: 'detailsLink',
+                    params: {
+                        article: this.articleList[0]
+                    }
+                });
+                window.open(routeUrl.href, '_blank');
+            }
+        }
     }
 </script>
 
--- src/views/admin/Login.vue
+++ src/views/admin/Login.vue
@@ -7,10 +7,10 @@
         <el-form ref="loginForm" :model="form" :rules="rules" label-width="80px" class="login-box">
             <h3 class="login-title">欢迎登录</h3>
             <el-form-item label="账号" prop="username">
-                <el-input type="text" placeholder="请输入账号" v-model="form.username"/>
+                <el-input type="text" placeholder="请输入账号" v-model="form.username"></el-input>
             </el-form-item>
             <el-form-item label="密码" prop="password">
-                <el-input type="password" placeholder="请输入密码" v-model="form.password"/>
+                <el-input type="password" placeholder="请输入密码" v-model="form.password"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" v-on:click="onSubmit('loginForm')">登录</el-button>

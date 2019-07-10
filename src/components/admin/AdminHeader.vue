4766a590c8b37d98e0b3f00463bdaab3696df4dd
xupingwei <mailxpw@gmail.com>
2019-07-11 00:06:17 +0800

    优化创建文章页面

--- src/components/admin/AdminHeader.vue
+++ src/components/admin/AdminHeader.vue
@@ -1,23 +1,63 @@
 <template>
     <div class="admin-header">
         <el-header>
-            <h3><i class="el-icon-s-home"></i>发布文章后台管理</h3>
-            <div style="float: right;margin-top: 10px;">
-                <el-dropdown :split-button="true"
-                             trigger="click"
-                             @command="handleCommand">
+            <el-row>
+                <el-col :span="6">
+                    <h3><i class="el-icon-s-home"></i>发布文章后台管理</h3>
+                </el-col>
+                <el-col :span="8">
+                    <el-menu
+                            :default-active="$route.path"
+                            text-color="#ffffff"
+                            mode="horizontal"
+                            active-text-color="#3f3f3f"
+                            background-color="#20A0FF">
+                        <el-menu-item index="/createArticle">
+                            <router-link tag="li" :to="{name:'createLink'}">
+                                <div class="head-menu"><i class="el-icon-edit"></i>
+                                    <span slot="title">文章编辑器</span>
+                                </div>
+                            </router-link>
+                        </el-menu-item>
+                        <el-menu-item index="/articleManager">
+                            <router-link tag="li" :to="{name:'articleManagerLink'}">
+                                <div class="head-menu">
+                                    <i class="el-icon-document"></i>
+                                    <span slot="title">文章管理</span>
+                                </div>
+                            </router-link>
+                        </el-menu-item>
+                        <el-menu-item index="/tagManager">
+                            <router-link tag="li" :to="{name:'tagManagerLink'}">
+                                <div class="head-menu">
+                                    <i class="el-icon-price-tag"></i>
+                                    <span slot="title"> 分类管理</span>
+                                </div>
+                            </router-link>
+                        </el-menu-item>
+                    </el-menu>
+                </el-col>
+
+
+                <el-col :span="6" :offset="4">
+                    <div style="float: right;margin-top: 10px;">
+                        <el-dropdown :split-button="true"
+                                     trigger="click"
+                                     @command="handleCommand">
                           <span class="el-dropdown-link">
                             <i class="el-icon-user el-icon--left"></i> {{username}}{{role}}
                           </span>
-                    <el-dropdown-menu slot="dropdown" style="width: 15%">
-                        <el-dropdown-item icon="el-icon-s-home" command="lookupHome">
-                            <span style="margin-left: 30px">查看主页</span>
-                        </el-dropdown-item>
-                        <el-dropdown-item icon="el-icon-s-tools" command="logout">
-                            <span style="margin-left: 30px">退出登录</span></el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-            </div>
+                            <el-dropdown-menu slot="dropdown" style="width: 15%">
+                                <el-dropdown-item icon="el-icon-s-home" command="lookupHome">
+                                    <span style="margin-left: 30px">查看主页</span>
+                                </el-dropdown-item>
+                                <el-dropdown-item icon="el-icon-s-tools" command="logout">
+                                    <span style="margin-left: 30px">退出登录</span></el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                    </div>
+                </el-col>
+            </el-row>
         </el-header>
     </div>
 </template>
@@ -71,5 +111,13 @@
     header h3 i {
         width: 30px;
         height: 30px;
+    }
+
+    .head-menu {
+        color: white;
+    }
+
+    .head-menu i{
+        color: white;
     }
 </style>
--- src/views/admin/Publish.vue
+++ src/views/admin/Publish.vue
@@ -1,47 +1,7 @@
 <template>
     <div class="publish">
         <app-admin-header></app-admin-header>
-        <el-radio-group v-model="isCollapse">
-            <el-radio-button :label="false">展开</el-radio-button>
-            <el-radio-button :label="true">收起</el-radio-button>
-        </el-radio-group>
-        <!--        <div class="lookup-home">-->
-        <!--            <router-link tag="a" target="_blank" :to="{name:'homeLink'}">-->
-        <!--                <div><i class="el-icon-s-home"></i>-->
-        <!--                    <span slot="title">查看主页</span>-->
-        <!--                </div>-->
-        <!--            </router-link>-->
-        <!--        </div>-->
         <div class="publish-content">
-            <el-menu
-                    :collapse="isCollapse"
-                    :default-active="$route.path"
-                    text-color="#20A0FF"
-                    class="el-menu-vertical-demo">
-                <el-menu-item index="/createArticle">
-                    <router-link tag="a" :to="{name:'createLink'}">
-                        <div><i class="el-icon-edit"></i>
-                            <span slot="title">文章编辑器</span>
-                        </div>
-                    </router-link>
-                </el-menu-item>
-                <el-menu-item index="/articleManager">
-                    <router-link tag="a" :to="{name:'articleManagerLink'}">
-                        <div>
-                            <i class="el-icon-document"></i>
-                            <span slot="title">文章管理</span>
-                        </div>
-                    </router-link>
-                </el-menu-item>
-                <el-menu-item index="/tagManager">
-                    <router-link tag="a" :to="{name:'tagManagerLink'}">
-                        <div>
-                            <i class="el-icon-price-tag"></i>
-                            <span slot="title"> 分类管理</span>
-                        </div>
-                    </router-link>
-                </el-menu-item>
-            </el-menu>
             <router-view></router-view>
         </div>
     </div>
@@ -56,7 +16,7 @@
         },
         data() {
             return {
-                isCollapse: true
+                // isCollapse: true
             }
         }
     }
@@ -81,7 +41,7 @@
     }
 
     .publish-content {
-        display: flex;
+        margin: 10px;
     }
 
     .el-menu {
